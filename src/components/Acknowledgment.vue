216909f638b60ea9dafa0315fabaebbba4692a56
Karolina <karolina@Karolinas-MacBook-Pro.local>
2024-03-13 22:11:27 +0100

    warning modal - missing file

---
+++ src/components/Acknowledgment.vue
@@ -0,0 +1,80 @@
+<template>
+  <div
+    id="popup-modal"
+    tabindex="-1"
+    :class="hiddenClass"
+    class="flex overflow-y-auto overflow-x-hidden absolute top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full"
+  >
+    <div class="relative p-4 w-full max-w-md max-h-full">
+      <div class="relative bg-gray-50 rounded-lg shadow dark:bg-black">
+        <button
+          type="button"
+          class="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+          data-modal-hide="popup-modal"
+        >
+          <svg
+            class="w-2 h-2"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 14 14"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+            />
+          </svg>
+        </button>
+        <div class="p-4 md:p-5 text-center">
+          <svg
+            class="mx-auto mb-4 text-gray-400 w-6 h-6 dark:text-gray-200"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 20 20"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+            />
+          </svg>
+          <h3 class="mb-5 text-sm font-medium text-gray-900 dark:text-white">
+            Parameters range is not restricted to match your device
+            computational power. Feel free to experiment, but increase them
+            carefully, otherwise your browser will freeze. Remember to decrease
+            max. radius as you increase the number of particles.
+          </h3>
+          <button
+            @click="acknowledge()"
+            data-modal-hide="popup-modal"
+            type="button"
+            class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center"
+          >
+            I understand
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { computed, ref } from "vue";
+
+const acknowledged = ref(window.localStorage.getItem("acknowledged") || false);
+
+async function acknowledge() {
+  window.localStorage.setItem("acknowledged", true);
+  acknowledged.value = true;
+}
+
+const hiddenClass = computed(() => ({
+  hidden: acknowledged.value,
+}));
+</script>
